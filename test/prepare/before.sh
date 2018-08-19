d838f9110cfa3177d5c17abd72f4bd35b1c7048a
Yumainaura <yuma.inaura@gmail.com>
2018-08-19 22:33:20 +0900

    Fix test

--- test/prepare/before.sh
+++ test/prepare/before.sh
@@ -6,13 +6,14 @@ export VIMNOW_PATH="$test_save_dir"
 rm -rf ~/tmp/vimnow/test # Do not use variable because missing remove is so dangerous
 
 # For Mac OS
-if [ $(uname) = 'Darwin']; then
+if [ $(uname) = 'Darwin' ]; then
   alias stat='gstat'
   alias date='gdate'
   alias tee='gtee'
   alias split='gspit'
+  alias echo='gecho'
 fi
 
 shopt -s expand_aliases
-alias vim="echo 'VIM EDITING'"
+alias vim="echo 'VIM EDITING'; echo"
 
