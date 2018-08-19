b3cb687e8d1a47abdce1acc98a8472115a0be117
Yumainaura <yuma.inaura@gmail.com>
2018-08-19 20:18:16 +0900

    Refactor test : echo OK

--- test/record_lastest_file.sh
+++ test/record_lastest_file.sh
@@ -4,6 +4,6 @@ source ./test/prepare/before.sh
 
 source ./bin/vimnow
 
-test $(ls "${test_save_dir}.latest_filepath") && echo .latest_filepath created
+test $(ls "${test_save_dir}.latest_filepath") && echo OK
 
 source ./test/prepare/after.sh
