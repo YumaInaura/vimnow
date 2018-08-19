6791c8408ebb8007fdd186bca149438eb76437c8
Yumainaura <yuma.inaura@gmail.com>
2018-08-19 20:21:09 +0900

    Update : create Executable file when script

--- test/record_lastest_file.sh
+++ test/record_lastest_file.sh
@@ -4,6 +4,6 @@ source ./test/prepare/before.sh
 
 source ./bin/vimnow
 
-test $(ls "${test_save_dir}.latest_filepath") && echo OK
+test $(ls "${test_save_dir}/.latest_filepath") && echo OK
 
 source ./test/prepare/after.sh
