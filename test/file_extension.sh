728c298d61416f5c30f4f0d0e2aa7c516785cf3a
Yumainaura <yuma.inaura@gmail.com>
2018-08-21 06:24:43 +0900

    Improve : vimnow echo filename

--- test/file_extension.sh
+++ test/file_extension.sh
@@ -2,15 +2,14 @@
 
 source ./test/prepare/before.sh
 
-./bin/vimnow --sh
+readonly filepath=$(./bin/vimnow --sh)
 
 readonly files=$(ls "$test_save_dir" | grep '\.sh')
 
 test $(echo "$files" | wc -l) = 1 && echo OK
 
-readonly latest_filepath=$(cat "${test_save_dir}/.latest_filepath")
-test $(stat -c '%a' "$latest_filepath") == '755' && echo OK
+test $(stat -c '%a' "$filepath") == '755' && echo OK
 
-[[ "$latest_filepath" =~ \.sh ]] && echo OK
+[[ "$filepath" =~ \.sh ]] && echo OK
 
 source ./test/prepare/after.sh
