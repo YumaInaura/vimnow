b061ac44441ba24e9d66e2583e738dd7471688f1
Yumainaura <yuma.inaura@gmail.com>
2018-08-21 19:53:36 +0900

    Fix test file_extension

--- test/file_extension.sh
+++ test/file_extension.sh
@@ -5,11 +5,7 @@ source ./test/prepare/before.sh
 readonly command_result=$(./bin/vimnow --sh)
 readonly created_file_path="$command_result"
 
-readonly files=$(ls "$test_save_dir" | grep '\.sh')
-
-test $(echo "$files" | wc -l) = 1 && echo OK
-
-test $(stat -c '%a' "$created_file_path") == '755' && echo OK
+test "$(stat -c '%a' $created_file_path)" = '755' && echo OK
 
 [[ "$created_file_path" =~ \.sh ]] && echo OK
 
