8347227fde9749d2be4eb8d723338d49b7ce494e
Yumainaura <yuma.inaura@gmail.com>
2018-08-21 19:11:08 +0900

    Refacoctor install.sh download_tmp_dir variable

--- install.sh
+++ install.sh
@@ -1,21 +1,23 @@
 #!/bin/bash -eu
 
 readonly github_code_base_url='https://raw.githubusercontent.com/YumaInaura/vimnow/master/bin/vimnow'
-curl -o .vimnow_donwnload "$github_code_base_url"?$((RANDOM))$((RANDOM))
 
-chmod +x .vimnow_donwnload
+readonly download_tmp_dir="$HOME"/.vimnow_download
+curl -o "$download_tmp_dir"/.vimnow_donwnload "$github_code_base_url"?$((RANDOM))$((RANDOM))
 
-mv .vimnow_donwnload /usr/local/bin/vimnow
+chmod +x "$download_tmp_dir".vimnow_donwnload
+
+mv "$download_tmp_dir".vimnow_donwnload /usr/local/bin/vimnow
 
 readonly vimnow_home="$HOME"/vimnow
 mkdir -p "$vimnow_home"
 mkdir -p "$vimnow_home"/save
 
 rm -rf "$HOME"/vimnow/bin
-cp -rf './bin' "$vimnow_home"
+cp -rf "$download_tmp_dir"/bin "$vimnow_home"
 
 rm -rf "$HOME"/vimnow/lib
-cp -rf './lib' "$vimnow_home"
+cp -rf "$download_tmp_dir"/lib "$vimnow_home"
 
 echo "vimnow installed"
 
