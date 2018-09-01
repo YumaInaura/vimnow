35df9e0f978f42be62beea9f68a51b64a81833dd
Yumainaura <yuma.inaura@gmail.com>
2018-09-01 14:02:36 +0900

    bin/vimnow to lib/vimnow

--- install.sh
+++ install.sh
@@ -3,18 +3,11 @@
 readonly vimnow_home_dir="$HOME"/vimnow
 mkdir -p "$vimnow_home_dir"
 
-rm -f /usr/local/bin/vimnow
-cp -r ./bin/vimnow /usr/local/bin/vimnow
-
 rm -rf "$vimnow_home_dir"/lib
 cp -rf ./lib "$vimnow_home_dir"/lib
 
-rm -rf "$vimnow_home_dir"/bin
-cp -rf ./bin "$vimnow_home_dir"/bin
-
-readonly command_path=/usr/local/bin/vimnow
-rm -f "$command_path"
-ln -s "$HOME"/vimnow/bin/vimnow "$command_path"
+rm -f /usr/local/bin/vimnow
+ln -s "$HOME"/vimnow/lib/vimnow /usr/local/bin/vimnow
 
 # Create data directory
 mkdir -p "$vimnow_home_dir"/save
